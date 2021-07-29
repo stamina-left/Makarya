f52bbd6e80c826d253988993b0ca33bd91785b6c
Albert Pangestu <stamina.left@gmail.com>
2021-07-29 23:43:27 +0700

    Initial Commit

---
+++ Makarya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Makarya
+//
+//  Created by Albert Pangestu on 29/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
