0166bc5e7ece0e5f3fb61459d527a21073b1dcec
Albert Pangestu <stamina.left@gmail.com>
2021-09-15 20:01:31 +0700

    TimerEntity with init & equatable.

--- Makarya/Enterprises/Entities/TimerEntity.swift
+++ Makarya/Enterprises/Entities/TimerEntity.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+final class TimerEntity: Equatable {
+    
+    static func == (lhs: TimerEntity, rhs: TimerEntity) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    var id: UUID
+    var date: Calendar
+    var duration: TimeInterval
+    var passed: TimeInterval
+    
+    init(id: UUID, date: Calendar, duration: TimeInterval, passed: TimeInterval) {
+        
+        self.id = id
+        self.date = date
+        self.duration = duration
+        self.passed = passed
+    }
+}
