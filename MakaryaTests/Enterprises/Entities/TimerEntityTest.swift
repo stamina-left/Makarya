64d9ac4e62352133b84e13bf218cdb7e45ffc04a
Albert Pangestu <stamina.left@gmail.com>
2021-09-21 00:20:19 +0700

    Added function to create a yesterday's date.

--- MakaryaTests/Enterprises/Entities/TimerEntityTest.swift
+++ MakaryaTests/Enterprises/Entities/TimerEntityTest.swift
@@ -37,13 +37,37 @@ class TimerEntityTest: XCTestCase {
     func testCheckTimerInput_withPastDate() {
         
         let identifier = UUID()
-        let pastDate = Date()
+        let pastDate = Date.yesterday
         let duration = TimeInterval(1_450.0)
         let passed = TimeInterval(1_450.0)
         
         let sut = try? TimerEntity(id: identifier, date: pastDate, duration: duration, passed: passed)
         
-        XCTAssertEqual(sut.date, nil, "Date is overdue.")
+        XCTAssertEqual(sut?.date, nil, "Date is not overdue.")
     }
+    
+}
 
+// MARK: - Helper
+
+extension Date {
+    
+    static var yesterday: Date { return Date().dayBefore }
+    static var tomorrow:  Date { return Date().dayAfter }
+    
+    var dayBefore: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: noon)!
+    }
+    var dayAfter: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
+    }
+    var noon: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+    }
+    var month: Int {
+        return Calendar.current.component(.month,  from: self)
+    }
+    var isLastDayOfMonth: Bool {
+        return dayAfter.month != month
+    }
 }
