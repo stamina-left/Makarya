07aa19ca710191dfe72fed0a324bf561a23a6535
Albert Pangestu <stamina.left@gmail.com>
2021-11-04 01:18:53 +0700

    Created new tests named ClockInteractorTests.

---
+++ MakaryaTests/Rules/Application/Interactor/ClockInteractorTests.swift
@@ -0,0 +1,15 @@
+//
+//  ClockInteractorTests.swift
+//  MakaryaTests
+//
+//  Created by Albert Pangestu on 04/11/21.
+//
+
+import XCTest
+@testable import Makarya
+
+class ClockInteractorTests: XCTestCase {
+
+    
+
+}
