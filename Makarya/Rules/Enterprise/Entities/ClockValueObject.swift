3588b60d2c6f0744174548fe7fea553dd6577bd3
Albert Pangestu <stamina.left@gmail.com>
2021-11-04 15:05:14 +0700

    Removed optional mark at the initialization.

--- Makarya/Rules/Enterprise/Entities/ClockValueObject.swift
+++ Makarya/Rules/Enterprise/Entities/ClockValueObject.swift
@@ -12,7 +12,7 @@ struct ClockValueObject: Equatable {
     let minutes: Int
     let seconds: Int
     
-    init?(hours: Int, minutes: Int, seconds: Int) {
+    init(hours: Int, minutes: Int, seconds: Int) {
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
