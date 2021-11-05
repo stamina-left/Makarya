da7c8019c501461381eb8e1d3d8d487ec8eb9ce9
Albert Pangestu <stamina.left@gmail.com>
2021-11-05 10:52:53 +0700

    New file named TimerInteractor, & added the protocol & the implementation.

---
+++ Makarya/Rules/Application/Interactor/TimerInteractor.swift
@@ -0,0 +1,19 @@
+//
+//  TimerInteractor.swift
+//  Makarya
+//
+//  Created by Albert Pangestu on 05/11/21.
+//
+
+import Foundation
+
+protocol TimerInteractor {
+    func setTimer(hours: Int, minutes: Int, seconds: Int)
+}
+
+final class TimerInteractorImplementation: TimerInteractor {
+    
+    func setTimer(hours: Int, minutes: Int, seconds: Int) {
+        <#code#>
+    }
+}
