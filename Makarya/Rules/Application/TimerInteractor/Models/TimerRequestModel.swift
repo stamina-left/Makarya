2ea564be9eb4fb146929c6147e73e053b84f9376
Albert Pangestu <stamina.left@gmail.com>
2021-11-11 00:20:59 +0700

    Removed enum on TimerRequestModel because we can use string to set the TimerEntity's state.

--- Makarya/Rules/Application/TimerInteractor/Models/TimerRequestModel.swift
+++ Makarya/Rules/Application/TimerInteractor/Models/TimerRequestModel.swift
@@ -12,23 +12,5 @@ struct TimerRequestModel {
     let minutes: Int
     let seconds: Int
     let date: Date
-    let state: TimerStateRequestModel
-}
-
-extension TimerRequestModel {
-    
-    enum TimerStateRequestModel: String {
-        case started = "started"
-        case paused = "paused"
-        case stopped = "stopped"
-        case finished = "finished"
-        
-        static func ==(left: TimerStateRequestModel, right: TimerEntity.TimerState) -> Bool {
-            return left.rawValue == right.rawValue
-        }
-        
-        static func ==(left: TimerEntity.TimerState, right: TimerStateRequestModel) -> Bool {
-            return left.rawValue == right.rawValue
-        }
-    }
+    let state: String
 }
