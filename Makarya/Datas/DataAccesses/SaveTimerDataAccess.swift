cf9838073bcc275f8d43d98c73eedcfe99c6c2b3
Albert Pangestu <stamina.left@gmail.com>
2021-11-14 15:21:12 +0700

    New Data Access named SaveTimerDataAccess with default implementation using Core Data.

---
+++ Makarya/Datas/DataAccesses/SaveTimerDataAccess.swift
@@ -0,0 +1,19 @@
+//
+//  SaveTimerDataAccess.swift
+//  Makarya
+//
+//  Created by Albert Pangestu on 14/11/21.
+//
+
+import Foundation
+
+protocol SaveTimerDataAccess {
+    func execute(timer: TimerEntity, completion: @escaping (Result<TimerEntity, Error>) -> Void)
+}
+
+final class CoreDataSaveTimerDataAccess: SaveTimerDataAccess {
+    
+    func execute(timer: TimerEntity, completion: @escaping (Result<TimerEntity, Error>) -> Void) {
+        <#code#>
+    }
+}
