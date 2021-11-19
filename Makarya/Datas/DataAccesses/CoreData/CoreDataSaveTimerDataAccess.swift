91dd021d06b56b55aee6e9375a09927e7b3435a0
Albert Pangestu <stamina.left@gmail.com>
2021-11-19 20:47:58 +0700

    Deleted appDelegate & UIKit because we can get ManagedObjectContext without it.

--- Makarya/Datas/DataAccesses/CoreData/CoreDataSaveTimerDataAccess.swift
+++ Makarya/Datas/DataAccesses/CoreData/CoreDataSaveTimerDataAccess.swift
@@ -5,16 +5,14 @@
 //  Created by Albert Pangestu on 17/11/21.
 //
 
-import UIKit
+import Foundation
 import CoreData
 
 final class CoreDataSaveTimerDataAccess: SaveTimerDataAccess {
     
+    var managedObjectContext: NSManagedObjectContext?
+    
     func execute(timer: TimerEntity, completion: @escaping (Result<TimerEntity, Error>) -> Void) {
         
-        guard let appDelegate =
-        UIApplication.shared.delegate as? AppDelegate else {
-            return
-        }
     }
 }
