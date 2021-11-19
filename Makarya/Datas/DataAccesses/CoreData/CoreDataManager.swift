45b0e805d15eac9aef2ff0a4e839c997d43de138
Albert Pangestu <stamina.left@gmail.com>
2021-11-20 00:39:08 +0700

    Created property named "modelName" & initialization of it.

--- Makarya/Datas/DataAccesses/CoreData/CoreDataManager.swift
+++ Makarya/Datas/DataAccesses/CoreData/CoreDataManager.swift
@@ -9,4 +9,13 @@ import Foundation
 
 final class CoreDataManager {
     
+    // MARK: - Properties
+    
+    private let modelName: String
+    
+    // MARK: - Initialization
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
 }
