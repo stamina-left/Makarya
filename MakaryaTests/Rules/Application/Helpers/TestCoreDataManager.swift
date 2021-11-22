4e9dacddbe55e44a7147941f388a453eb913cc3c
Albert Pangestu <stamina.left@gmail.com>
2021-11-22 18:36:30 +0700

    Create a function that returns a CoreDataManager with unit testing specification (in-memory store).

---
+++ MakaryaTests/Rules/Application/Helpers/TestCoreDataManager.swift
@@ -0,0 +1,22 @@
+//
+//  TestCoreDataManager.swift
+//  MakaryaTests
+//
+//  Created by Albert Pangestu on 22/11/21.
+//
+
+import XCTest
+import CoreData
+import Makarya
+
+class TestCoreDataManager {
+
+    func create() -> CoreDataManager {
+        
+        let persistentDescription = NSPersistentStoreDescription()
+        persistentDescription.type = NSInMemoryStoreType
+        
+        
+    }
+
+}
