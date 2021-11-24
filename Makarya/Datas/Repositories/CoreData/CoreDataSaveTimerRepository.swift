1f2083f18433067d156e8fc2ad8493c5327ac10e
Albert Pangestu <stamina.left@gmail.com>
2021-11-24 20:28:45 +0700

    Removed To-Do because successfully saved doesn't need to return anything.

--- Makarya/Datas/Repositories/CoreData/CoreDataSaveTimerRepository.swift
+++ Makarya/Datas/Repositories/CoreData/CoreDataSaveTimerRepository.swift
@@ -39,7 +39,6 @@ final class CoreDataSaveTimerRepository: SaveTimerRepository {
             completion(.failure(error))
         }
         
-        // TO-DO: Convert Timer CoreData model into TimerEntity.
         completion(.success(()))
     }
     
