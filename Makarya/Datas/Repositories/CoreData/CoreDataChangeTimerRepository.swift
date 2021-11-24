3564c989bc0642269cf6433c87e99d7d06378bad
Albert Pangestu <stamina.left@gmail.com>
2021-11-24 21:34:00 +0700

    Create a coreDataManager with default modelName "CoreDataTimer", & SQLite store.

--- Makarya/Datas/Repositories/CoreData/CoreDataChangeTimerRepository.swift
+++ Makarya/Datas/Repositories/CoreData/CoreDataChangeTimerRepository.swift
@@ -6,8 +6,15 @@
 //
 
 import Foundation
+import CoreData
 
 final class CoreDataChangeTimerRepository: ChangeTimerRepository {
+    
+    private let coreDataManager: CoreDataManager
+    
+    init(coreDataManager: CoreDataManager = CoreDataManager(modelName: "CoreDataTimer", storeType: NSSQLiteStoreType)) {
+        self.coreDataManager = coreDataManager
+    }
     
     func execute(timer: TimerEntity, completion: @escaping (Result<TimerEntity, Error>) -> Void) {
         <#code#>
