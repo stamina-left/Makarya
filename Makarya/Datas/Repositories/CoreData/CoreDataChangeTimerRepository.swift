60310938df33265e663770c047a659634691b1f0
Albert Pangestu <stamina.left@gmail.com>
2021-11-24 21:13:13 +0700

    Added CoreDataChangeTimerRepository that conforms ChangeTimerRepository protocol (implementation will be added later).

---
+++ Makarya/Datas/Repositories/CoreData/CoreDataChangeTimerRepository.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataChangeTimerRepository.swift
+//  Makarya
+//
+//  Created by Albert Pangestu on 24/11/21.
+//
+
+import Foundation
+
+final class CoreDataChangeTimerRepository: ChangeTimerRepository {
+    
+    func execute(timer: TimerEntity, completion: @escaping (Result<TimerEntity, Error>) -> Void) {
+        <#code#>
+    }
+}
