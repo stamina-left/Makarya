3c573c3434054e4569efda8f9fc326435e09c578
Albert Pangestu <stamina.left@gmail.com>
2021-12-07 14:50:03 +0700

    Renamed from SetTimerInteractor into AddableTimerInteractor.

---
+++ Makarya/Domain/Interactors/TimerInteractor/AddableTimerInteractor.swift
@@ -0,0 +1,57 @@
+//
+//  AddableTimerInteractor.swift
+//  Makarya
+//
+//  Created by Albert Pangestu on 05/11/21.
+//
+
+import Foundation
+
+protocol AddableInteractor {
+    
+    typealias AddableTimerInteractorCompletion = (Result<Entity, Error>) -> Void
+    func add(_ request: Entity, completion: @escaping (AddableTimerInteractorCompletion))
+}
+
+struct AddableTimerInteractor: AddableInteractor {
+    
+    func add(_ request: Entity, completion: @escaping (AddableTimerInteractorCompletion)) {
+        <#code#>
+    }
+}
+
+//final class SetTimerInteractorImplementation {
+//
+//    private let repository: SaveTimerRepository
+//
+//    init(repository: SaveTimerRepository) {
+//        self.repository = repository
+//    }
+//
+//    func execute(request: TimerRequest,
+//                 completion: @escaping (Result<TimerResponse, Error>) -> Void) {
+//
+//        do {
+//
+//            try ClockValueObjectValidation().validate(request)
+//
+//            let clock = ClockValueObject(hours: request.hours,
+//                                         minutes: request.minutes,
+//                                         seconds: request.seconds)
+//            let timer = TimerEntity(clock: clock,
+//                                    date: request.date)
+//
+//            repository.execute(timer: timer) { result in
+//                switch result {
+//                case .success():
+//                    let response = TimerResponseModel(timer: timer)
+//                    completion(.success(response))
+//                case .failure(let error):
+//                    completion(.failure(error))
+//                }
+//            }
+//        } catch {
+//            completion(.failure(error))
+//        }
+//    }
+//}
